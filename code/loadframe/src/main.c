01fabf77ad956c4bcd6cb7a371178361554a686d
rattboi <rattboi@gmail.com>
2013-05-21 22:59:38 -0700

    Blinking works, some buttons work, both screens work

--- code/loadframe/src/main.c
+++ code/loadframe/src/main.c
@@ -29,6 +29,54 @@ __CRP const unsigned int CRP_WORD = CRP_NO_CRP ;
 
 // TODO: insert other definitions and declarations here
 
+int ProcessToggleButton(int pin)
+{
+
+#define INTEGRATOR_MAX 500
+
+	static int integrator[2] = {0,0};
+	static int output[2] = {1,1};
+
+	int button_state;
+
+	button_state = (!! LPC_GPIO[1]->MASKED_ACCESS[1 << (pin + 5)]);
+
+	if (button_state == 0)
+	{
+		if (integrator[pin] > 0)
+			integrator[pin]--;
+	}
+	else if (integrator[pin] < INTEGRATOR_MAX)
+		integrator[pin]++;
+
+	if (integrator[pin] == 0)
+	{
+		output[pin] = 0;
+	}
+	else if (integrator[pin] >= INTEGRATOR_MAX)
+	{
+		integrator[pin] = INTEGRATOR_MAX;
+		output[pin] = 1;
+	}
+	return !(output[pin]);
+}
+
+int GetButton(int pin)
+{
+	static int button[6];
+	static int last_state[6] ;
+	static int state[6];
+
+	state[pin] = ProcessToggleButton(pin);
+
+	if (state[pin] == 1 && last_state[pin] != 1 )
+	{
+		button[pin] = button[pin] ^ 1;
+	}
+
+	last_state[pin] = state[pin];
+}
+
 int main(void) {
 	GPIOInit();
 	dac_init();
@@ -43,27 +91,46 @@ int main(void) {
     GPIOSetValue( 0,4,1);
 	// TODO: insert code here
 
+    GPIOSetDir( 1, 5, 0);
+    GPIOSetDir( 1, 6, 0);
+
 	// Enter an infinite loop, just incrementing a counter
 	volatile static uint16_t sample = 0 ;
-  adc_channels adc_data;
-    int i;
+	adc_channels adc_data;
+    int i = 0;
+    int left,right;
+    int left_last,right_last;
     uint32_t sum;
     uint32_t sum2;
+    const int count = 1024;
     while(1) {
-		sum = 0;
-		sum2 = 0;
-		for (i = 0; i < 32; i++) {
-          adc_data = adc_read();
-          sum += adc_data.loadcell;
-          sum2 += adc_data.lvdt;
+    	i++;
+		adc_data = adc_read();
+		sum += adc_data.loadcell;
+		sum2 += adc_data.lvdt;
+		if (!(i % count))
+		{
+			sum /= count;
+			sum2 /= count;
+			SysTick->CTRL &=~1;
+			update_display(LOADC_DISP, sum);
+			update_display(LVDT_DISP,  sum2);
+			SysTick->CTRL |=1;
+			i = sum = sum2 = 0;
 		}
-		sum /= 32;
-		sum2 /= 32;
-		dac_send(sum);
-		SysTick->CTRL &=~1;
-		update_display(LOADC_DISP, sum);
-		update_display(LVDT_DISP,  sum2);
-		SysTick->CTRL |=1;
+		//dac_send(sum);
+
+		left =  ProcessToggleButton(0);
+		right = ProcessToggleButton(1);
+
+		if (left && !left_last)
+			blink_left();
+
+		if (right && !right_last)
+			blink_right();
+
+		left_last = left;
+		right_last = right;
 	}
 	return 0 ;
 }
--- code/loadframe/src/sseg.c
+++ code/loadframe/src/sseg.c
@@ -13,46 +13,59 @@
 #define  NUM_DIGS 5
 #define  NUM_DISPLAYS 2
 
+typedef struct { uint8_t PORT;
+				 uint8_t SHIFT_OE;
+			     uint8_t SHIFT_SDI;
+			     uint8_t SHIFT_CLK;
+			     uint8_t SHIFT_LE;
+			     uint8_t COUNT_CP;
+			     uint8_t COUNT_MR;
+			     uint8_t mode;
+			     uint16_t count;
+			     uint8_t blink_digit;
+} SSEG;
+
+SSEG sseg[NUM_DISPLAYS] = {
+		{ LOADC_PORT,
+		    LOADC_SHIFT_OE,
+		    LOADC_SHIFT_SDI,
+		    LOADC_SHIFT_CLK,
+		    LOADC_SHIFT_LE,
+		    LOADC_COUNT_CP,
+		    LOADC_COUNT_MR,
+			0,
+			0,
+			0},
+		{ LVDT_PORT,
+		    LVDT_SHIFT_OE,
+		    LVDT_SHIFT_SDI,
+		    LVDT_SHIFT_CLK,
+		    LVDT_SHIFT_LE,
+		    LVDT_COUNT_CP,
+		    LVDT_COUNT_MR,
+			1,
+			0,
+			0}
+};
+
 uint8_t writebuf [NUM_DISPLAYS][NUM_DIGS];
 
 void disp_off(uint32_t disp) {
-	switch (disp)
-	{
-	case LOADC_DISP:
-		GPIOSetValue(LOADC_PORT,LOADC_SHIFT_OE,1);
-		break;
-	case LVDT_DISP:
-		GPIOSetValue(LVDT_PORT,LVDT_SHIFT_OE,1);
-		break;
-	}
+	GPIOSetValue(sseg[disp].PORT,sseg[disp].SHIFT_OE,1);
 }
+
 void sendval(uint32_t disp, char value) {
 	int i;
-	switch (disp)
-	{
-	case LOADC_DISP:
-		GPIOSetValue(LOADC_PORT,LOADC_SHIFT_OE,0);
-		for (i = 0; i < 8; i++) {
-			GPIOSetValue(LOADC_PORT,LOADC_SHIFT_SDI, !!(value & 0x80));
-			value = value << 1;
-			GPIOSetValue(LOADC_PORT,LOADC_SHIFT_CLK, 1);
-			GPIOSetValue(LOADC_PORT,LOADC_SHIFT_CLK, 0);
-		}
-		GPIOSetValue(LOADC_PORT,LOADC_SHIFT_LE, 1);
-		GPIOSetValue(LOADC_PORT,LOADC_SHIFT_LE, 0);
-		break;
-	case LVDT_DISP:
-		GPIOSetValue(LVDT_PORT,LVDT_SHIFT_OE,0);
-		for (i = 0; i < 8; i++) {
-			GPIOSetValue(LVDT_PORT,LVDT_SHIFT_SDI, !!(value & 0x80));
-			value = value << 1;
-			GPIOSetValue(LVDT_PORT,LVDT_SHIFT_CLK, 1);
-			GPIOSetValue(LVDT_PORT,LVDT_SHIFT_CLK, 0);
-		}
-		GPIOSetValue(LVDT_PORT,LVDT_SHIFT_LE, 1);
-		GPIOSetValue(LVDT_PORT,LVDT_SHIFT_LE, 0);
-		break;
+
+	GPIOSetValue(sseg[disp].PORT,sseg[disp].SHIFT_OE,0);
+	for (i = 0; i < 8; i++) {
+		GPIOSetValue(sseg[disp].PORT,sseg[disp].SHIFT_SDI, !!(value & 0x80));
+		value = value << 1;
+		GPIOSetValue(sseg[disp].PORT,sseg[disp].SHIFT_CLK, 1);
+		GPIOSetValue(sseg[disp].PORT,sseg[disp].SHIFT_CLK, 0);
 	}
+	GPIOSetValue(sseg[disp].PORT,sseg[disp].SHIFT_LE, 1);
+	GPIOSetValue(sseg[disp].PORT,sseg[disp].SHIFT_LE, 0);
 }
 
 //converts a bcd number to 7seg value
@@ -89,6 +102,9 @@ uint8_t bcdto7seg(uint8_t num) {
 	case 9:
 		return 0b01100111;
 		break;
+	case 0xBB:
+		return 0;
+		break;
 	default:
 		return 0x40;
 	}
@@ -96,7 +112,8 @@ uint8_t bcdto7seg(uint8_t num) {
 
 void update_display(uint32_t disp, uint32_t num){
 	int i;
-	if (num < (1 << 16))
+
+	if (num < 100000)
 		for(i = 0; i < NUM_DIGS; i++) {
 			writebuf[disp][i] = bcdto7seg(num % 10);
 			num /= 10;
@@ -106,81 +123,81 @@ void update_display(uint32_t disp, uint32_t num){
 			writebuf[disp][i] = bcdto7seg(0xff);
 }
 
+void blink_left()
+{
+	if (sseg[1].blink_digit < 4)
+		sseg[1].blink_digit++;
+//	else
+//		sseg[1].blink_digit = 0;
+}
+
+void blink_right()
+{
+	if (sseg[1].blink_digit > 0)
+		sseg[1].blink_digit--;
+//	else
+//		sseg[1].blink_digit = 4;
+}
+
 void SysTick_Handler(void) {
-	static int character;
+	static int character = 0;
+	static int blinkrate = 0;
+	int i;
 
-	disp_off(LOADC_DISP);
-	disp_off(LVDT_DISP);
+	for (i = 0; i < NUM_DISPLAYS; i++)
+	{
+		disp_off(i);
 
-	switch (character) {
-	case 1:
-	case 2:
-	case 3:
-	case 4:
-		GPIOSetValue(LOADC_PORT, LOADC_COUNT_CP,0);
-		GPIOSetValue(LOADC_PORT, LOADC_COUNT_CP,1);
-		GPIOSetValue(LVDT_PORT, LVDT_COUNT_CP,0);
-		GPIOSetValue(LVDT_PORT, LVDT_COUNT_CP,1);
-	break;
-	case 0:
-		GPIOSetValue(LOADC_PORT, LOADC_COUNT_MR,1);
-		GPIOSetValue(LOADC_PORT, LOADC_COUNT_MR,0);
-		GPIOSetValue(LVDT_PORT, LVDT_COUNT_MR,1);
-		GPIOSetValue(LVDT_PORT, LVDT_COUNT_MR,0);
+		if (character == 0)
+		{
+			GPIOSetValue(sseg[i].PORT, sseg[i].COUNT_MR,1);
+			GPIOSetValue(sseg[i].PORT, sseg[i].COUNT_MR,0);
+		}
+		else
+		{
+			GPIOSetValue(sseg[i].PORT, sseg[i].COUNT_CP,0);
+			GPIOSetValue(sseg[i].PORT, sseg[i].COUNT_CP,1);
+		}
+		if (sseg[i].mode && sseg[i].blink_digit == character && blinkrate > 250)
+			sendval(i, 0);
+		else
+			sendval(i, writebuf[i][character]);
 	}
-	sendval(LOADC_DISP, writebuf[LOADC_DISP][character]);
-	sendval(LVDT_DISP,  writebuf[LVDT_DISP][character]);
+
 	character = ++character%5;
+	blinkrate = ++blinkrate%500;
 }
 
 int sseg_init(void) {
+	int i;
+
     GPIOInit();
 
-    // Set up Load Cell display I/O dirs
-    GPIOSetDir(LOADC_PORT, LOADC_SHIFT_OE, 1);
-    GPIOSetValue(LOADC_PORT, LOADC_SHIFT_OE,0);
+    for (i = 0; i < NUM_DISPLAYS; i++)
+    {
+		GPIOSetDir  (sseg[i].PORT, sseg[i].SHIFT_OE, 1);
+		GPIOSetValue(sseg[i].PORT, sseg[i].SHIFT_OE,0);
 
-	GPIOSetDir(LOADC_PORT, LOADC_SHIFT_SDI, 1);
-    GPIOSetValue(LOADC_PORT, LOADC_SHIFT_SDI,1);
+		GPIOSetDir  (sseg[i].PORT, sseg[i].SHIFT_SDI, 1);
+		GPIOSetValue(sseg[i].PORT, sseg[i].SHIFT_SDI,1);
 
-	GPIOSetDir(LOADC_PORT, LOADC_SHIFT_CLK, 1);
-	GPIOSetValue(LOADC_PORT, LOADC_SHIFT_CLK,0);
+		GPIOSetDir  (sseg[i].PORT, sseg[i].SHIFT_CLK, 1);
+		GPIOSetValue(sseg[i].PORT, sseg[i].SHIFT_CLK,0);
 
-	GPIOSetDir(LOADC_PORT, LOADC_SHIFT_LE, 1);
-	GPIOSetValue(LOADC_PORT, LOADC_SHIFT_LE,0);
+		GPIOSetDir  (sseg[i].PORT, sseg[i].SHIFT_LE, 1);
+		GPIOSetValue(sseg[i].PORT, sseg[i].SHIFT_LE,0);
 
-	GPIOSetDir(LOADC_PORT, LOADC_COUNT_MR, 1);
-	GPIOSetValue(LOADC_PORT, LOADC_COUNT_MR,0);
-	GPIOSetValue(LOADC_PORT, LOADC_COUNT_MR,1);
-	GPIOSetValue(LOADC_PORT, LOADC_COUNT_MR,0);
+		GPIOSetDir  (sseg[i].PORT, sseg[i].COUNT_MR, 1);
+		GPIOSetValue(sseg[i].PORT, sseg[i].COUNT_MR,0);
+		GPIOSetValue(sseg[i].PORT, sseg[i].COUNT_MR,1);
+		GPIOSetValue(sseg[i].PORT, sseg[i].COUNT_MR,0);
 
-	GPIOSetDir(LOADC_PORT, LOADC_COUNT_CP, 1);
+		GPIOSetDir(sseg[i].PORT, sseg[i].COUNT_CP, 1);
 
-    // Set up LVDT display I/O dirs
-    GPIOSetDir(LVDT_PORT, LVDT_SHIFT_OE, 1);
-    GPIOSetValue(LVDT_PORT, LVDT_SHIFT_OE,0);
+		update_display(i, 0xfffff);
+    }
 
-	GPIOSetDir(LVDT_PORT, LVDT_SHIFT_SDI, 1);
-    GPIOSetValue(LVDT_PORT, LVDT_SHIFT_SDI,1);
-
-	GPIOSetDir(LVDT_PORT, LVDT_SHIFT_CLK, 1);
-	GPIOSetValue(LVDT_PORT, LVDT_SHIFT_CLK,0);
-
-	GPIOSetDir(LVDT_PORT, LVDT_SHIFT_LE, 1);
-	GPIOSetValue(LVDT_PORT, LVDT_SHIFT_LE,0);
-
-	GPIOSetDir(LVDT_PORT, LVDT_COUNT_MR, 1);
-	GPIOSetValue(LVDT_PORT, LVDT_COUNT_MR,0);
-	GPIOSetValue(LVDT_PORT, LVDT_COUNT_MR,1);
-	GPIOSetValue(LVDT_PORT, LVDT_COUNT_MR,0);
-
-	GPIOSetDir(LVDT_PORT, LVDT_COUNT_CP, 1);
-
-	update_display(LOADC_DISP, 0xfffff);
-	update_display(LVDT_DISP,  0xfffff);
-	SysTick_Handler();
-
-    SysTick->LOAD = 8000000 / 60 - 1;
+    SysTick->LOAD = 8000000 / 150 - 1;
     SysTick->VAL = 0;
     SysTick->CTRL = SysTick_CTRL_ENABLE_Msk |
             SysTick_CTRL_TICKINT_Msk |
